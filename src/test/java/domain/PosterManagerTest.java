0ac2c46a34e6267066aec88e1bf30e693dbf9906
UBCh <u2b127@gmail.ru>
2021-11-12 14:05:12 +0300

    the percentage of coverage on the branch is 90

--- src/test/java/domain/PosterManagerTest.java
+++ src/test/java/domain/PosterManagerTest.java
@@ -5,67 +5,91 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PosterManagerTest {
+public class PosterManagerTest {
 
     PosterManager service = new PosterManager();
 
     {
     }
 
+
+
     @Test
     public void shouldGetPoster() {
-        PosterManager service = new PosterManager(5);
+        PosterManager service = new PosterManager();
+        String[] poster = {"bloodshot", "forward", "hotelBelgrade", "gentlemen", "InvisibleMan", "trollsWorldTour", "numberOne"
+        };
         String[] expected = {"bloodshot", "forward", "hotelBelgrade", "gentlemen", "InvisibleMan", "trollsWorldTour", "numberOne"};
-        String[] actual = service.getPoster();
+        String[] actual = service.getPoster(poster);
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void addMovie() {
-        PosterManager service = new PosterManager(5);
+        PosterManager service = new PosterManager();
+        String[] poster = {"bloodshot", "forward", "hotelBelgrade", "gentlemen", "InvisibleMan", "trollsWorldTour", "numberOne"
+        };
         String[] expected = {"bloodshot", "forward", "hotelBelgrade", "gentlemen", "InvisibleMan", "trollsWorldTour", "numberOne", "ZERO"};
-        String[] actual = service.addMovie("ZERO");
+        String[] actual = service.addMovie("ZERO", poster);
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void showAddedMovies_one() {
         PosterManager service = new PosterManager(5);
-        service.addMovie("ZERO");
-        String[] expected = {"ZERO", "numberOne", "trollsWorldTour", "InvisibleMan", "gentlemen"};
-        String[] actual = service.showAddedMovies();
+        String[] poster = {"bloodshot", "forward", "hotelBelgrade", "gentlemen", "InvisibleMan", "trollsWorldTour", "numberOne"
+        };
+        String[] expected = {"numberOne", "trollsWorldTour", "InvisibleMan", "gentlemen", "hotelBelgrade"};
+        String[] actual = service.showAddedMovies(poster);
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void showAddedMovies_two() {
         PosterManager service = new PosterManager(10);
-        service.addMovie("ZERO");
-        String[] expected = {"ZERO", "numberOne", "trollsWorldTour", "InvisibleMan", "gentlemen", "hotelBelgrade", "forward", "bloodshot"};
-        String[] actual = service.showAddedMovies();
+        String[] poster = {"bloodshot", "forward", "hotelBelgrade", "gentlemen", "InvisibleMan", "trollsWorldTour", "numberOne"
+        };
+        String[] expected = {"numberOne", "trollsWorldTour", "InvisibleMan", "gentlemen", "hotelBelgrade", "forward", "bloodshot"};
+        String[] actual = service.showAddedMovies(poster);
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void showAddedMovies_three() {
         PosterManager service = new PosterManager(11);
-        service.addMovie("ZERO");
-        service.addMovie("ZERO");
-        service.addMovie("ZERO");
-        service.addMovie("ZERO");
-        String[] expected = {"ZERO", "ZERO", "ZERO","ZERO", "numberOne", "trollsWorldTour", "InvisibleMan", "gentlemen", "hotelBelgrade", "forward"};
-        String[] actual = service.showAddedMovies();
+        String[] poster = {"bloodshot", "forward", "hotelBelgrade", "gentlemen", "InvisibleMan", "trollsWorldTour", "numberOne"
+        };
+        String[] expected = {"numberOne", "trollsWorldTour", "InvisibleMan", "gentlemen", "hotelBelgrade", "forward", "bloodshot"};
+        String[] actual = service.showAddedMovies(poster);
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void showAddedMovies_four() {
         PosterManager service = new PosterManager(0);
-        service.addMovie("ZERO");
-        String[] expected = {"ZERO"};
-        String[] actual = service.showAddedMovies();
+        String[] poster = {"bloodshot", "forward", "hotelBelgrade", "gentlemen", "InvisibleMan", "trollsWorldTour", "numberOne"
+        };
+        String[] expected = {"numberOne"};
+        String[] actual = service.showAddedMovies(poster);
         assertArrayEquals(expected, actual);
     }
-
+    @Test
+    public void showAddedMovies_five() {
+        PosterManager service = new PosterManager(11);
+        String[] poster = {"bloodshot", "forward", "hotelBelgrade", "gentlemen", "InvisibleMan", "trollsWorldTour", "numberOne", "ZORRO", "ZORRO", "ZORRO", "ZORRO", "ZORRO"
+        };
+        String[] expected = {"ZORRO", "ZORRO", "ZORRO","ZORRO","ZORRO","numberOne", "trollsWorldTour", "InvisibleMan", "gentlemen", "hotelBelgrade"};
+        String[] actual = service.showAddedMovies(poster);
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void showAddedMovies_six() {
+        PosterManager service = new PosterManager(1);
+        String[] poster = {"bloodshot", "forward", "hotelBelgrade", "gentlemen", "InvisibleMan", "trollsWorldTour", "numberOne"
+        };
+        String[] expected = {"numberOne"};
+        String[] actual = service.showAddedMovies(poster);
+        assertArrayEquals(expected, actual);
+    }
 }
 
