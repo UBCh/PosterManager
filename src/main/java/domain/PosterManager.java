dcfe678242272814c35969270466013d69446be4
UBCh <u2b127@gmail.ru>
2021-11-12 14:05:12 +0300

    the percentage of coverage on the branch is 80

--- src/main/java/domain/PosterManager.java
+++ src/main/java/domain/PosterManager.java
@@ -1,25 +1,25 @@
 package domain;
 
 public class PosterManager {
-    public String title;
+    private String title;
     private int lengthReturn;
-    private int j;
-    String[] poster = {"bloodshot", "forward", "hotelBelgrade", "gentlemen", "InvisibleMan", "trollsWorldTour", "numberOne"};
+    private final int maxReturn = 10;
+    private String[] poster;
 
     public PosterManager(int lengthReturn) {
         this.lengthReturn = lengthReturn;
     }
 
     public PosterManager() {
-
     }
 
-    public String[] getPoster() {
+
+    public String[] getPoster(String[] poster) {
         return poster;
     }
 
 
-    public String[] addMovie(String title) {
+    public String[] addMovie(String title, String[] poster) {
         String[] tmp = new String[poster.length + 1];
         for (int i = 0; i < poster.length; i++) {
             tmp[i] = poster[i];
@@ -30,10 +30,10 @@ public class PosterManager {
         return poster;
     }
 
-    public String[] showAddedMovies() {
+    public String[] showAddedMovies(String[] poster) {
 
-            if (lengthReturn > 10) {
-            lengthReturn = 10;
+        if (lengthReturn > maxReturn) {
+            lengthReturn = maxReturn;
         }
         if (lengthReturn > poster.length) {
             lengthReturn = poster.length;
@@ -45,19 +45,19 @@ public class PosterManager {
         int refund = 0;
         String[] tmp = new String[lengthReturn];
 
-        if (poster.length < 10) {
+        if (poster.length < maxReturn) {
             refund = poster.length;
         }
-       if (poster.length > 10) {
-           refund = lengthReturn;
-       }
+        if (poster.length > lengthReturn) {
+            refund = lengthReturn;
+        }
         if (lengthReturn == 1) {
             tmp[0] = poster[poster.length - 1];
         }
 
         int indexTo = 0;
         for (int indexFrom = 0; indexFrom < lengthReturn; indexFrom++) {
-            tmp[indexTo] = poster[poster.length -(1+indexTo)];
+            tmp[indexTo] = poster[poster.length - (1 + indexTo)];
             indexTo++;
             if (indexTo == refund) break;
         }
--- src/test/java/domain/PosterManagerTest.java
+++ src/test/java/domain/PosterManagerTest.java
@@ -5,65 +5,61 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PosterManagerTest {
+public class PosterManagerTest {
 
     PosterManager service = new PosterManager();
 
     {
     }
 
+    String[] poster = {"bloodshot", "forward", "hotelBelgrade", "gentlemen", "InvisibleMan", "trollsWorldTour", "numberOne"
+    };
+
     @Test
     public void shouldGetPoster() {
-        PosterManager service = new PosterManager(5);
+        PosterManager service = new PosterManager();
         String[] expected = {"bloodshot", "forward", "hotelBelgrade", "gentlemen", "InvisibleMan", "trollsWorldTour", "numberOne"};
-        String[] actual = service.getPoster();
+        String[] actual = service.getPoster(poster);
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void addMovie() {
-        PosterManager service = new PosterManager(5);
+        PosterManager service = new PosterManager();
         String[] expected = {"bloodshot", "forward", "hotelBelgrade", "gentlemen", "InvisibleMan", "trollsWorldTour", "numberOne", "ZERO"};
-        String[] actual = service.addMovie("ZERO");
+        String[] actual = service.addMovie("ZERO", poster);
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void showAddedMovies_one() {
         PosterManager service = new PosterManager(5);
-        service.addMovie("ZERO");
-        String[] expected = {"ZERO", "numberOne", "trollsWorldTour", "InvisibleMan", "gentlemen"};
-        String[] actual = service.showAddedMovies();
+        String[] expected = {"numberOne", "trollsWorldTour", "InvisibleMan", "gentlemen", "hotelBelgrade"};
+        String[] actual = service.showAddedMovies(poster);
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void showAddedMovies_two() {
         PosterManager service = new PosterManager(10);
-        service.addMovie("ZERO");
-        String[] expected = {"ZERO", "numberOne", "trollsWorldTour", "InvisibleMan", "gentlemen", "hotelBelgrade", "forward", "bloodshot"};
-        String[] actual = service.showAddedMovies();
+        String[] expected = {"numberOne", "trollsWorldTour", "InvisibleMan", "gentlemen", "hotelBelgrade", "forward", "bloodshot"};
+        String[] actual = service.showAddedMovies(poster);
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void showAddedMovies_three() {
         PosterManager service = new PosterManager(11);
-        service.addMovie("ZERO");
-        service.addMovie("ZERO");
-        service.addMovie("ZERO");
-        service.addMovie("ZERO");
-        String[] expected = {"ZERO", "ZERO", "ZERO","ZERO", "numberOne", "trollsWorldTour", "InvisibleMan", "gentlemen", "hotelBelgrade", "forward"};
-        String[] actual = service.showAddedMovies();
+        String[] expected = {"numberOne", "trollsWorldTour", "InvisibleMan", "gentlemen", "hotelBelgrade", "forward", "bloodshot"};
+        String[] actual = service.showAddedMovies(poster);
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void showAddedMovies_four() {
         PosterManager service = new PosterManager(0);
-        service.addMovie("ZERO");
-        String[] expected = {"ZERO"};
-        String[] actual = service.showAddedMovies();
+        String[] expected = {"numberOne"};
+        String[] actual = service.showAddedMovies(poster);
         assertArrayEquals(expected, actual);
     }
 
